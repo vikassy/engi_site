97add6319f0f92a4289aed0611fceb5cb900ff4b
vikas <vikasyaligar.it@gmail.com>
2013-07-12 23:49:13 +0530

    Added mail settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,12 @@
 ActionMailer::Base.raise_delivery_errors = true
 require "#{Rails.root}/lib/development_mail_interceptor"
 ActionMailer::Base.smtp_settings = {
+  :openssl_verify_mode  => OpenSSL::SSL::VERIFY_NONE,
   :address              => "mail.engineer.org.in",
   :port                 => 26,
   :domain               => "www.engineer.org.in",
   :user_name            => "donotreply@engineer.org.in",
+  :password 			=> "Engi2013@NiTk",
   :authentication       => :login,
   :enable_starttls_auto => true
 }
